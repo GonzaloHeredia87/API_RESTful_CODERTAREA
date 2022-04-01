b715e185b7024d68e43bb5d7b07eb76c8bc39961
gonzalo <gherediam87@gmail.com>
2022-03-31 23:02:43 -0300

    avance, metodos terminados, get, post, delete. Falta revisar put y la insserción mediante el index.html

---
+++ api/Producto.js
@@ -0,0 +1,67 @@
+class Producto{
+
+    static item = 0;
+
+    constructor(){
+        this.persona = [];
+        this.objeto = [];
+
+    }
+
+    crear(title, price, thumbnail){
+        const persona = {
+            title: title,
+            price: price,
+            thumbnail: thumbnail,
+            id : Producto.item+1
+        }
+        this.objeto.push(persona);
+        Producto.item ++;
+        return persona;
+    }
+
+    buscar(id){
+        const persona = {error:'Producto no encontrado'};
+        for(let i =0; this.objeto.length>=i; i++){
+            if (this.objeto[i].id==id){
+                persona = this.objeto[i];
+            }
+        }
+        return persona;
+    }
+
+    editar(id, title, price, thumbnail){
+        const persona = {};
+        if (this.objeto.length>0){
+            for (let i = 0; this.objeto.length>=i;i++){
+                if(this.objeto[i].id == id){
+                    this.objeto[i].title = title;
+                    this.objeto[i].price = price;
+                    this.objeto[i].thumbnail = thumbnail;
+                    persona = this.objeto[i];
+                }
+            }
+        }
+    }
+
+    borra(id){
+        let estado = false;
+        if (this.objeto.length>0){
+            for(let i = 0; this.objeto.length>i;i++){
+                if (this.objeto[i].id==id){
+                    this.objeto.splice(i, 1);
+                    estado = true;
+                }
+            }
+        }
+        console.log('estado de borrar: '+ estado);
+        return estado;
+    }
+
+    obtenerTodo(){
+        return this.objeto;
+    }
+
+}
+
+module.exports = Producto;
--- servidor.js
+++ servidor.js
@@ -2,13 +2,37 @@ const express = require('express');
 const {Router} = express;
 const app = express();
 const productoRouter = Router();
-const producto = "require(./api/producto.js)";
+app.use(express.static('public'))
+const producto = require("./api/Producto.js");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 const PORT = 8888;
+const Productos = new producto()
+productoRouter.use('/api', productoRouter);
 
+productoRouter.get('/', (req, res)=>{
+    console.log('Hola estoy en productos');
+    
+    console.log(Productos.obtenerTodo());
+    res.json(Productos.obtenerTodo());
+});
 
+productoRouter.post('/', (req, res)=>{
+    console.log(req.query);
+    const data = req.query;
+    const resultado = Productos.crear(data.title, data.price, data.thumbnail);
+    res.json(resultado);
+});
+productoRouter.put('/:id', (req, res)=>{
+    console.log(req.params.id);
+    res.json("in put");
+});
 
+productoRouter.delete('/:id', (req, res)=>{
+    const index = req.params.id;
+    res.json(Productos.borra(index));
+});
+app.use('/productos', productoRouter);
 
 const server = app.listen(PORT, ()=>{
     console.log('servidor http en el puerto 8888');
